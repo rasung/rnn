e8abaecc35628935e2212765159954b489654a36
xotjr <xotjrrl@naver.com>
2017-09-06 09:55:04 +0900

    test code

--- trainer/task.py
+++ trainer/task.py
@@ -1,73 +1,130 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
 import csv
+import argparse
+import os
 
-CSV_DATA_PATH_LIST = '1month_1month_19970101_20170831.csv'
+if __name__ == '__main__':
 
-f_data = open(CSV_DATA_PATH_LIST, 'r', encoding='utf-8')
-rdr = csv.reader(f_data)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train-file',
+        required=True
+    )
 
-hidden_size = 10
-batch_size = 1
-input_sequence_length = 30  
-output_sequence_length = 1
-input_num_classes = 18
-output_num_classes = 18
+    parser.add_argument(
+        '--job-name',
+        required=True
+    )
 
-X = tf.placeholder(tf.int32, [None, input_sequence_length])
-X_one_hot = tf.one_hot(X, input_num_classes)
-print("X_one_hot", X_one_hot)  # check out the shape
+    parser.add_argument(
+        '--job-dir',
+        required=True
+    )
+    args = parser.parse_args()
+    arguments = args.__dict__
 
-Y = tf.placeholder(tf.int32, [None, output_sequence_length])  # 1
-Y_one_hot = tf.one_hot(Y, output_num_classes)  # one hot
-print("Y_one_hot", Y_one_hot)
-Y_one_hot = tf.reshape(Y_one_hot, [-1, output_num_classes])
-print("Y_reshape", Y_one_hot)
-
-
-# Make a lstm cell with hidden_size (each unit output vector size)
-def lstm_cell():
-    cell = rnn.BasicLSTMCell(hidden_size, state_is_tuple=True)
-    return cell
-
-multi_cells = rnn.MultiRNNCell([lstm_cell() for _ in range(2)], state_is_tuple=True)
-
-# outputs: unfolding size x hidden size, state = hidden size
-outputs, _states = tf.nn.dynamic_rnn(multi_cells, X_one_hot, dtype=tf.float32)
-# reshape out
-outputs = tf.reshape(outputs, [batch_size, hidden_size * input_sequence_length])
+    pathToJobDir = arguments.pop('job_dir')
+    jobName = arguments.pop('job_name')
+    pathToData = arguments.pop('train_file')
 
 
-W = tf.Variable(tf.random_normal([hidden_size * input_sequence_length, output_num_classes]), name='weight')
-b = tf.Variable(tf.random_normal([output_num_classes]), name='bias')
+    csv_file = pathToData
+    hidden_size = 5
+    batch_size = 1
+    input_sequence_length = 30  
+    output_sequence_length = 1
+    input_num_classes = 18
+    output_num_classes = 18
+    stack = 5
 
-# tf.nn.softmax computes softmax activations
-logits = tf.matmul(outputs, W) + b
-hypothesis = tf.nn.softmax(logits)
+    X = tf.placeholder(tf.int32, [None, input_sequence_length])
+    X_one_hot = tf.one_hot(X, input_num_classes)
+    print("X_one_hot", X_one_hot)  # check out the shape
 
-# Cross entropy cost/loss
-cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
-                                                 labels=Y_one_hot)
-cost = tf.reduce_mean(cost_i)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+    Y = tf.placeholder(tf.int32, [None, output_sequence_length])  # 1
+    Y_one_hot = tf.one_hot(Y, output_num_classes)  # one hot
+    print("Y_one_hot", Y_one_hot)
+    Y_one_hot = tf.reshape(Y_one_hot, [-1, output_num_classes])
+    print("Y_reshape", Y_one_hot)
 
-prediction = tf.argmax(hypothesis, 1)
-correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    # Make a lstm cell with hidden_size (each unit output vector size)
+    def lstm_cell():
+        cell = rnn.BasicLSTMCell(hidden_size, state_is_tuple=True)
+        return cell
 
-    for line in rdr:
-        dataX = []
-        dataY = []
-    
-        # if testing max value = dataX.append(line[0:-2]) dataY.append(line[-2:-1])
-        # else if testing min value = dataX.append(line[0:-2]) dataY.append(line[-1:])
-        dataX.append(line[0:-2])
-        dataY.append(line[-2:-1])
+    multi_cells = rnn.MultiRNNCell([lstm_cell() for _ in range(stack)], state_is_tuple=True)
 
-        print(dataX, dataY)
+    # outputs: unfolding size x hidden size, state = hidden size
+    outputs, _states = tf.nn.dynamic_rnn(multi_cells, X_one_hot, dtype=tf.float32)
+    # reshape out
+    outputs = tf.reshape(outputs, [batch_size, hidden_size * input_sequence_length])
+
+
+    W1 = tf.Variable(tf.random_normal([hidden_size * input_sequence_length, hidden_size * input_sequence_length]), name='weight1')
+    b1 = tf.Variable(tf.random_normal([hidden_size * input_sequence_length]), name='bias1')
+
+    outputs = tf.matmul(outputs, W1) + b1
+
+    W2 = tf.Variable(tf.random_normal([hidden_size * input_sequence_length, output_num_classes]), name='weight2')
+    b2 = tf.Variable(tf.random_normal([output_num_classes]), name='bias2')
+
+    # tf.nn.softmax computes softmax activations
+    logits = tf.matmul(outputs, W2) + b2
+    hypothesis = tf.nn.softmax(logits)
+
+    # Cross entropy cost/loss
+    cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                    labels=Y_one_hot)
+    cost = tf.reduce_mean(cost_i)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+    prediction = tf.argmax(hypothesis, 1)
+    correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+    filename_queue = tf.train.string_input_producer([csv_file])
+    key, value = tf.TextLineReader().read(filename_queue)
+
+    input_list = []
+    for l in range(input_sequence_length + 1 + 1):
+        input_list.append([1])
+
+    data = tf.decode_csv(value, record_defaults=input_list)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        i=0
+        while i < 100:
+            i+=1
+            datas = sess.run(data)
         
-        _, loss, accur = sess.run([optimizer, cost, accuracy], feed_dict={X: dataX, Y: dataY})
-        print(loss, accur)
+            dataX = []
+            dataY = []
+            # if testing max value = dataX.append(datas[0:-2]) dataY.append(datas[-2:-1])
+            # else if testing min value = dataX.append(datas[0:-2]) dataY.append(datas[-1:])
+            dataX.append(datas[0:-2])
+            dataY.append(datas[-2:-1])
+
+            print(i, dataX, dataY)
+            
+            _, loss, accur, hypo = sess.run([optimizer, cost, accuracy, hypothesis], feed_dict={X: dataX, Y: dataY})
+
+            hypo_list=[]
+            for j in range(len(hypo[0])):
+                hypo_list.append(round(hypo[0][j], 3))
+
+            print(loss, accur, hypo_list)
+
+        coord.request_stop()
+        coord.join(threads)
+
+        saver = tf.train.Saver()
+        model_file = os.path.join(pathToJobDir, jobName)
+        saver.save(sess, model_file, global_step=0)
